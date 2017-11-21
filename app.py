cc94f3e41ca0acdf6b25ff4453c55db6d8c891a6
Carol217 <cpan3@stuy.edu>
2017-11-21 09:18:12 -0500

    retrieve api key

--- app.py
+++ app.py
@@ -12,10 +12,33 @@ import urllib2
 
 app = Flask (__name__)
 
+'''
+use the following code to retrieve keys (specify as needed)
+'''
+with open("weatherapi.txt", "r") as w:
+    weather_key = w.readline().rstrip()
+
+with open("eventapi.txt", "r") as e:
+    book_key = e.readline().rstrip()
+    local_key = e.readline().rstrip()
+    tourist_key = e.readline().rstrip()
+    
+    
+#home route, takes zipcode and other inputs
 @app.route("/")
 def hello_world():
+    print "**DIAG: has api key been acquired?**"
+    print "The weather api key is " + weather_key
+    print "The weather api key is " + book_key
+    print "The weather api key is " + local_key
+    print "The weather api key is " + tourist_key
     return "Yo what up."
 
-if __name__ = "__main__":
-    app.debug == True
+#reach out to apis and acquire info
+@app.route("/info")
+def display_info():
+    return "Yo what up."
+
+if __name__ == "__main__":
+    app.debug = True
     app.run()

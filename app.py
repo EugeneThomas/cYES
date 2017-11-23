66c9c9bfce3bcf18a4478ca8d8f6e5f26917c272
Valery Chen <cpan3@stuy.edu>
2017-11-22 21:45:11 -0500

    api calls for weather and books

--- app.py
+++ app.py
@@ -9,19 +9,10 @@ P01 -- ArRESTed Development
 from flask import Flask, render_template, request
 from flask import redirect, flash, url_for
 import urllib2
+import json
+from util import api_calls as api
 
 app = Flask (__name__)
-
-'''
-use the following code to retrieve keys (specify as needed)
-'''
-with open("weatherapi.txt", "r") as w:
-    weather_key = w.readline().rstrip()
-
-with open("eventapi.txt", "r") as e:
-    book_key = e.readline().rstrip()
-    local_key = e.readline().rstrip()
-    tourist_key = e.readline().rstrip()
     
     
 #home route, takes zipcode and other inputs
@@ -29,15 +20,25 @@ with open("eventapi.txt", "r") as e:
 def hello_world():
     print "**DIAG: has api key been acquired?**"
     print "The weather api key is " + weather_key
-    print "The weather api key is " + book_key
-    print "The weather api key is " + local_key
-    print "The weather api key is " + tourist_key
-    return "Yo what up."
+    print "The book api key is " + book_key
+    print "The eventbrite api key is " + local_key
+    print "The tripadvisor api key is " + tourist_key
+    return render_template("home.html")
 
 #reach out to apis and acquire info
 @app.route("/info")
 def display_info():
-    return "Yo what up."
+    #retrieving weather data
+    zipcode=request.args["zip"]
+    #code for api call have been moved to util/api_calls.py
+    wform = api.weathercall(zipcode)
+
+    #retrieving book data
+    age=request.args["age"]
+    #code for api call have been moved to util/api_calls.py
+    bform = api.bookcall(age)
+    
+    return render_template("info.html", bookdata=bform, weatherdata=wform)
 
 if __name__ == "__main__":
     app.debug = True
